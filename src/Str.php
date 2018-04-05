a69832f5db546986060446d19c74b0a8eabeac65
Ronan Chilvers <ronan@d3r.com>
2018-04-05 21:27:57 +0100

    More methods and test cases
    Add pascal, camel, snake and truncate methods
    Add test cases for the above

--- src/Str.php
+++ src/Str.php
@@ -58,32 +58,124 @@ class Str
             return $plural;
         }
 
-        $string = strtolower($string);
+        $string = mb_strtolower($string);
         if (isset(static::$plurals[$string])) {
             return static::$plurals[$string];
         }
 
         // [1.0]
-        if (!in_array(substr($string, -2, 1), static::$vowels) && 'y' == substr($string, -1)) {
-            return substr($string, 0, -1) . 'ies';
+        if (!in_array(mb_substr($string, -2, 1), static::$vowels) && 'y' == mb_substr($string, -1)) {
+            return mb_substr($string, 0, -1) . 'ies';
         }
 
         // [2.0]
-        if ('f' == substr($string, -1) || 'fe' == substr($string, -2)) {
-            $length = ('e' == substr($string, -1)) ? -2 : -1 ;
-            return substr($string, 0, $length) . 'ves';
+        if ('f' == mb_substr($string, -1) || 'fe' == mb_substr($string, -2)) {
+            $length = ('e' == mb_substr($string, -1)) ? -2 : -1 ;
+            return mb_substr($string, 0, $length) . 'ves';
         }
 
         // [3.1]
-        if (!in_array(substr($string, -2, 1), static::$vowels) && 'o' == substr($string, -1)) {
+        if (!in_array(mb_substr($string, -2, 1), static::$vowels) && 'o' == mb_substr($string, -1)) {
             return $string . 'es';
         }
 
         // [3.2]
-        if (in_array(substr($string, -2), ['ch', 'sh', 'ss']) || in_array(substr($string, -1), ['s', 'x', 'z'])) {
+        if (in_array(mb_substr($string, -2), ['ch', 'sh', 'ss']) || in_array(mb_substr($string, -1), ['s', 'x', 'z'])) {
             return $string . 'es';
         }
 
         return $string . 's';
+    }
+
+    /**
+     * Convert a string to PascalCase
+     *
+     * Pascal case is upper camel case, or camel case with the first
+     * letter uppercased.
+     *
+     * @param string $string
+     * @param array $allowed Allowed characters that won't be compressed
+     * @return string
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    static public function pascal($string, $allowed = [])
+    {
+        $string = mb_strtolower($string);
+        $string = preg_replace('#[^a-z0-9' . implode('', $allowed) . ']+#', ' ', $string);
+        $string = str_replace(' ', '', ucwords($string));
+
+        return $string;
+    }
+
+    /**
+     * Convert a string to camelCase
+     *
+     * We're taking camel case here to mean lower camel case, ie: the first
+     * letter is lower case.
+     *
+     * @param string $string
+     * @param array $allowed Allowed characters that won't be compressed
+     * @return string
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    static public function camel($string, $allowed = [])
+    {
+        return lcfirst(
+            static::pascal($string, $allowed)
+        );
+    }
+
+    /**
+     * Snake case a string
+     *
+     * This method snake cases phrases and can also convert camelCase
+     * and PascalCase strings to snake case.
+     *
+     * @param string $string
+     * @param array $allowed Allowed characters that won't be compressed
+     * @return string
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    static public function snake($string, $allowed = [])
+    {
+        // $string = mb_strtolower($string);
+        $string = preg_replace('#([A-Z]{1})#', ' $1', $string);
+        $string = mb_strtolower(trim($string));
+        $string = preg_replace('#[^a-z0-9' . implode('', $allowed) . ']{1,}#', ' ', $string);
+        $string = str_replace(' ', '_', $string);
+
+        return $string;
+    }
+
+    /**
+     * Truncate a string to a given length, optionally respecting word
+     * boundaries
+     *
+     * @param string $string The string to truncate
+     * @param int $length The length to truncate to
+     * @param string $suffix Suffix to tag on the end of the string
+     * @param boolean $words Respect word boundaries
+     * @param
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    static public function truncate($string, $length, $suffix = '...', $words = false)
+    {
+        if ($length > mb_strlen($string)) {
+            return $string;
+        }
+        if (false === $words) {
+            return mb_substr($string, 0, $length - mb_strlen($suffix)) . $suffix;
+        }
+        $matches = [];
+        preg_match(
+            "/(^.{1," . ($length - mb_strlen($suffix)) . "})(?=\s|$).*/u",
+            $string,
+            $matches
+        );
+        if (!isset($matches[1])) {
+            return mb_substr($string, 0, $length - mb_strlen($suffix)) . $suffix;
+        }
+
+        return $matches[1] . $suffix;
     }
 }
--- tests/StrTest.php
+++ tests/StrTest.php
@@ -8,7 +8,7 @@ use Ronanchilvers\Utility\Str;
 /**
  * Test case for Str string manipulations methods
  *
- * @author me
+ * @author Ronan Chilvers <ronan@d3r.com>
  */
 class StrTest extends TestCase
 {
@@ -89,5 +89,122 @@ class StrTest extends TestCase
     public function testExplicitPluralIsReturned($singular, $plural)
     {
         $this->assertEquals('foobar', Str::plural($singular, 2, 'foobar'));
+    }
+
+    /**
+     * Data provider for camel cased phrases
+     *
+     * @return array<array>
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function camelCaseProvider()
+    {
+        return [
+            ['my_phrase', 'myPhrase'],
+            ['one two three', 'oneTwoThree'],
+        ];
+    }
+
+    /**
+     * Test that Str::pascal() processes given phrases correctly
+     *
+     * @dataProvider camelCaseProvider
+     * @test
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function testGivenPhrasesArePascalCasedCorrectly($phrase, $camelCase)
+    {
+        $pascal = ucfirst($camelCase);
+        $this->assertEquals($pascal, Str::pascal($phrase));
+    }
+
+    /**
+     * Test that Str::camel() processes given phrases correctly
+     *
+     * @dataProvider camelCaseProvider
+     * @test
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function testGivenPhrasesAreCamelCasedCorrectly($phrase, $camelCase)
+    {
+        $this->assertEquals($camelCase, Str::camel($phrase));
+    }
+
+    /**
+     * Provider for snake cased phrases
+     *
+     * @return array<array>
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function snakeCaseProvider()
+    {
+        return [
+            ['one two three', 'one_two_three'],
+            ['one.two.three', 'one_two_three'],
+            ['oneTwoThree', 'one_two_three'],
+            ['OneTwoThree', 'one_two_three'],
+        ];
+    }
+
+    /**
+     * Test that Str::snake() can convert known phrases
+     *
+     * @dataProvider snakeCaseProvider
+     * @test
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function testGivenPhrasesAreSnakeCasedCorrectly($phrase, $snakeCase)
+    {
+        $this->assertEquals($snakeCase, Str::snake($phrase));
+    }
+
+    /**
+     * Test that truncating a string less than a given length works
+     *
+     * @test
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function testTruncateToLessThanLengthDoesNothing()
+    {
+        $this->assertEquals('foobar is great', Str::truncate('foobar is great', 100));
+    }
+
+    /**
+     * Test that truncate works for string adding the default suffix
+     *
+     * @test
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function testTruncateReturnsCorrectStringWithSuffix()
+    {
+        $result = Str::truncate('foobar is great', 10);
+        $this->assertEquals('foobar ...', $result);
+        $this->assertEquals(10, mb_strlen($result));
+    }
+
+    /**
+     * Test that passing a custom suffix works correctly
+     *
+     * @test
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function testTruncateWithCustomSuffixReturnsStringWithSuffix()
+    {
+        $result = Str::truncate('foobar is great', 10, '---');
+        $this->assertEquals('foobar ---', $result);
+        $this->assertEquals(10, mb_strlen($result));
+    }
+
+    /**
+     * Test that truncate breaks on words when asked to
+     *
+     * @test
+     * @author Ronan Chilvers <ronan@d3r.com>
+     */
+    public function testTruncateRespectsWordBoundaries()
+    {
+        $result = Str::truncate('foobar is great', 12, '...', true);
+        $this->assertEquals('foobar is...', $result);
+        $this->assertEquals(12, mb_strlen($result));
     }
 }
